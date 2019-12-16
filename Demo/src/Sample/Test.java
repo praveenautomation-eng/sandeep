87e732d556c5433e5ec6d628fb03d08b64c04935
praveenautomation-eng <praveennarra123@gmail.com>
2019-12-16 11:58:49 +0530

    added demo

---
+++ Demo/src/Sample/Test.java
@@ -0,0 +1,24 @@
+package Sample;
+
+public class Test 
+
+{
+	int a=10;
+	int b=100;
+	Test()
+	{
+		a=3330;
+		 b=2000;
+	}
+
+	public static void main(String[] args)
+	{
+		int a=30;
+	Test a1=new Test();
+		System.out.println(a1.a);
+		System.out.println(a1.b);
+		
+
+	}
+
+}
